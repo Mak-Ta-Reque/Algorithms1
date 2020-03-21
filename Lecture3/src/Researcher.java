57dace4856eae756c9f0e51e9bdd38acc9bd7bfb
abka03 <maktareq@gmail.com>
2020-03-21 11:03:36 +0100

    small upade

--- Lecture3/src/Researcher.java
+++ Lecture3/src/Researcher.java
@@ -1,6 +1,30 @@
-public class Researcher {
+public class Researcher implements Comparable<Researcher> {
     //This class creates object that has three files.
     private String name;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public int getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode = postalCode;
+    }
+
     private String city;
     private int postalCode;
     public Researcher() {
