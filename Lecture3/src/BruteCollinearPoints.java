5e91e63d70698df0cbf77f4def9d654a42caa72d
abka03 <maktareq@gmail.com>
2020-03-28 11:52:51 +0100

    Line segment brutforce complete

--- Lecture3/src/BruteCollinearPoints.java
+++ Lecture3/src/BruteCollinearPoints.java
@@ -10,6 +10,9 @@ public class BruteCollinearPoints {
        if (checkNull(points)) throw new IllegalArgumentException();
        if (points == null) throw new IllegalArgumentException();
        this.points = points;
+       numberOfSegments = 0;
+       lineSegment = new LineSegment[1];
+       populateLineSegments();
        
    }
    public LineSegment[] segments() {
@@ -20,11 +23,31 @@ public class BruteCollinearPoints {
    public int numberOfSegments() {
      return numberOfSegments;  
    }
+   private void populateLineSegments() {
+       int N = points.length - 1;
+       for (int p = 0; p <= N - 3; p++ ) {
+           for (int q = p + 1; q <= N - 2; q++ ) {
+               for (int r = q + 1; r <= N - 1; r++ ) {
+                   for (int s = r +1; s <=N; s++ ) {
+                       if (points[p].slopeTo(points[q]) == points[p].slopeTo(points[r]) && (points[p].slopeTo(points[q]) == (points[p].slopeTo(points[s])))) {
+                           lineSegment[numberOfSegments ++] = new LineSegment(points[p], points[s]);
+                           if (lineSegment.length <= numberOfSegments ) resize(numberOfSegments *2);
+                           
+                       }
+                       
+                      
+                               
+                   }
+                   
+               }
+           }
+       }
+   }
    
    //This method check duplicate
    private boolean duplicate(Point[] points) {
-       for (int i = 0; i < points.length -1; i++) {
-           for (int j = i+1; j < points.length -1; j++) {
+       for (int i = 0; i <= points.length -1; i++) {
+           for (int j = i+1; j <= points.length -1; j++) {
                if (points[i].compareTo(points[j]) == 0) return true;
            }
        }
@@ -32,9 +55,18 @@ public class BruteCollinearPoints {
    }
    //This method check if any point is null 
    private boolean checkNull(Point[] points) {
-       for (int j = 0; j < points.length -1; j++) {
+       for (int j = 0; j <= points.length -1; j++) {
            if (points[j] == null) return true;
        }
        return false;
+   }
+   
+   private void resize(int size) {
+      LineSegment [] newLineSegment =  new LineSegment[size];
+      for (int i = 0; i < lineSegment.length; i++ ) {
+          newLineSegment[i] = lineSegment[i];
+          
+      }
+      lineSegment = newLineSegment;
    }
 }
