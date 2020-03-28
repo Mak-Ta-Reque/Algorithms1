e26943a6c8f12cf15b7b2ca81416656c21f9bbf4
abka03 <maktareq@gmail.com>
2020-03-28 10:26:22 +0100

    line segment brutforce

--- Lecture3/src/BruteCollinearPoints.java
+++ Lecture3/src/BruteCollinearPoints.java
@@ -1,14 +1,40 @@
 
 public class BruteCollinearPoints {
+    
+   private final Point [] points;
+   private int numberOfSegments;
+   private LineSegment[] lineSegment;
+   
    public BruteCollinearPoints(Point[] points) {
-       // finds all line segments containing 4 point
-   }
-   public int numberOfSegments() {
-       // the number of line segments
-       return 1;
+       if (duplicate(points)) throw new IllegalArgumentException();
+       if (checkNull(points)) throw new IllegalArgumentException();
+       if (points == null) throw new IllegalArgumentException();
+       this.points = points;
+       
    }
    public LineSegment[] segments() {
        // the line segments
-       
+       return lineSegment;
+   }
+   
+   public int numberOfSegments() {
+     return numberOfSegments;  
+   }
+   
+   //This method check duplicate
+   private boolean duplicate(Point[] points) {
+       for (int i = 0; i < points.length -1; i++) {
+           for (int j = i+1; j < points.length -1; j++) {
+               if (points[i].compareTo(points[j]) == 0) return true;
+           }
+       }
+       return false;
+   }
+   //This method check if any point is null 
+   private boolean checkNull(Point[] points) {
+       for (int j = 0; j < points.length -1; j++) {
+           if (points[j] == null) return true;
+       }
+       return false;
    }
 }
