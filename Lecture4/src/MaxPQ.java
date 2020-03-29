b913d2c6a37f91b587417d746f402ad87e37e79c
abka03 <maktareq@gmail.com>
2020-03-30 00:05:14 +0200

    pq implementation

--- Lecture4/src/MaxPQ.java
+++ Lecture4/src/MaxPQ.java
@@ -1,4 +1,54 @@
 
-public class PriorityQueue {
+public final class MaxPQ<Key extends Comparable<Key>> {
+    private int size;
+    private Key[] pq;
+    
+    // Constructor 
+    MaxPQ(){
+        this.size = 0;
+        this.pq = (Key[]) new Object[1];
+        
+    }
+    
+    MaxPQ(Key [] keys){
+        // Implement later
+    }
+    
+    public void insert(Key v) {
+        
+    }
+    
+    public Key delMax() {
+        
+    }
+    
+    public boolean isEmpty() {
+        
+    }
+    
+    public Key Max() {
+        
+    }
+    
+    public int size() {
+        return this.size;
+    }
+    
+    private void resize(int newSize) {
+        Key [] newKeys = (Key[])new Object[newSize];
+        for(int k = 0; k < pq.length; k++ ) {
+              newKeys[k] = pq[k];
+        }
+        pq = newKeys;
+    }
+    
+    private void swim(int k) {
+        
+    }
+    
+    private void sink(int k) {
+        
+    }
+    
 
 }
