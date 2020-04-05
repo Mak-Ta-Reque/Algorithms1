5d9858f4206611a53dd4bcc0b2d63d72ed784d38
abka03 <maktareq@gmail.com>
2020-04-05 21:29:36 +0200

    puzzle solve

--- Lecture4/src/Board.java
+++ Lecture4/src/Board.java
@@ -1,5 +1,8 @@
 import edu.princeton.cs.algs4.StdOut;
 import java.lang.Math;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 public final class Board {
 
@@ -60,9 +63,8 @@ public final class Board {
                 if(tile == 0) continue;
                 int goalX = (tile -1) / N;
                 int goalY = (tile -1) - (N * goalX);
-                System.out.println("X " + goalX + " " + "Y " + goalY);
-                dist += Math.abs(goalY - j) + Math.abs(goalX-i);
-                //System.out.println(uniq_dist);
+                //System.out.println("X " + goalX + " " + "Y " + goalY);
+                dist += Math.abs(goalY - j) + Math.abs(goalX-i);             
             }
             
         }
@@ -71,18 +73,33 @@ public final class Board {
 
     // is this board the goal board?
     public boolean isGoal() {
-        return false;
+        if (hamming() == 0) return true;
+        else return false;
     }
 
     // does this board equal y?
     public boolean equals(Object y) {
-        return false;
+        if (this == y) return true;
+        if (this.getClass() != y.getClass())
+            return false;
+        Board that = (Board)y;
+        if (N != that.N) return false;
+        if (this.hamming() != that.hamming()) return false;
+        if(this.manhattan() != that.manhattan()) return false;
+        for (int i = 0; i < N; i++ ) {
+            for (int j = 0; j< N; j++) {
+                if(this.tiles[i][j] != that.tiles[i][j]) return false;
+            }
+        }
+        
+        
+        return true;
     }
 
     // all neighboring boards
-//    public Iterable<Board> neighbors(){
-//        return new Iterable<Board>();
-//    }
+    public Iterable<Board> neighbors(){
+        return new Neighbours();
+    }
 
     // a board that is obtained by exchanging any pair of tiles
 //    public Board twin() {
@@ -98,6 +115,104 @@ public final class Board {
         System.out.println(board.toString());
         System.out.println(board.hamming());
         System.out.println(board.manhattan());
+        for( Board it : board.neighbors()) {
+            System.out.println(it.toString());
+        }
+        
+    }
+    
+    private class Neighbours implements Iterable<Board>{
+
+        @Override
+        public Iterator<Board> iterator() {
+            // TODO Auto-generated method stub
+           return new NeighbourIterator();
+        }
+        
+    }
+    private final class NeighbourIterator implements Iterator<Board>{
+        
+        
+        private final LinkedList<Board> neighbor = new LinkedList<Board>();
+        private int zeroX;
+        private int zeroY;
+        private NeighbourIterator() {
+           
+            for (int i = 0; i < N; i++) {
+                boolean flag = false;
+                for (int j = 0; j < N; j++) {
+                    if(tiles[i][j] == 0 ) {
+                        zeroX = i;
+                        zeroY = j;
+                        flag = true;
+                        break;
+                    }
+                }
+                if(flag) {
+                    break;
+                }
+            }
+//            for (int i = 0; i <= 1; i++) {
+//                for (int j  = 0; j <= 1; j++) {
+//                    int n1X = zeroX + j;
+//                    int n1Y = zeroY;
+//                }
+            
+            
+            System.out.println("x " + zeroX + " " + "y " + zeroY);
+            
+            int n1X = zeroX +1;
+            int n1Y = zeroY;
+            System.out.println("x " + n1X + " " + "y " + n1Y);
+            addToList(n1X,n1Y);
+            
+            n1X = zeroX - 1;
+            n1Y = zeroY;
+            addToList(n1X,n1Y);
+            
+            n1X = zeroX;
+            n1Y = zeroY +1;
+            addToList(n1X,n1Y);
+            
+          
+            n1X = zeroX;
+            n1Y = zeroY -1;
+            addToList(n1X,n1Y);
+        }  
+            @Override
+            public boolean hasNext() {
+                // TODO Auto-generated method stub
+                
+                return neighbor.poll() !=  null;
+            }
+            @Override
+            public Board next() {
+                // TODO Auto-generated method stub
+                return neighbor.poll() ;
+            }
+            
+            
+        
+        private void addToList(int i, int j) {
+            if(i >= 0 && i < N && j >=0 && j < N) {
+                //System.out.println("x " + i + " " + "y " + j);
+                int [][] tilesN = Arrays.copyOf(tiles, tiles.length);
+                int temp = tilesN[i][j];
+                System.out.println("temp " + temp);
+                tilesN[i][j] = 0;
+                tilesN[zeroX][zeroY] = temp;
+                for(int k = 0; k < tiles.length; k++ ) {
+                    for (int l = 0; l < tiles.length; l++) {
+                        System.out.print(tiles[k][l]);
+                    }
+                    System.out.println();
+                }
+                Board board = new Board(tilesN);
+                System.out.println(board.toString());
+                neighbor.add(board);
+            }
+        }
+        
     }
 
 }
