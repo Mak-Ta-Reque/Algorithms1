4539dc48f888835bdb67a454bd19389fe774c86a
abka03 <maktareq@gmail.com>
2020-04-07 21:32:01 +0200

    rank

--- Lecture4/src/BSTST.java
+++ Lecture4/src/BSTST.java
@@ -6,9 +6,11 @@ public final class BSTST<Key extends Comparable<Key>, Value> {
         private Value value;
         private Node left;
         private Node right;
-        private Node(Key key, Value value) {
+        private int count;
+        private Node(Key key, Value value, int count) {
             this.key = key;
             this.value = value;
+            this.count = count;
         }
     }
     
@@ -18,14 +20,36 @@ public final class BSTST<Key extends Comparable<Key>, Value> {
     }
     
     private Node put(Node x, Key key, Value value) {
-        if (x == null) return new Node(key,value);
+        if (x == null) return new Node(key,value, 1);
         int cmp = key.compareTo(x.key);
         if (cmp < 0) x.left = put(x.left, key, value);
         else if (cmp > 0) x.right = put(x.right, key, value);
         else x.value = value;
+        x.count = 1 + size(x.left) + size(x.right);
         return x;
         
         
+    }
+    
+    public int size() {
+       return size(root);
+        
+    }
+    private int size(Node x) {
+        if(x == null) return 0;
+        return x.count;
+    }
+    
+    public int rank(Key key) {
+        return rank(key, root);
+    }
+    
+    private int rank(Key key, Node node) {
+         if(node == null) return 0;
+         int cmp = key.compareTo(node.key);
+         if (cmp < 0) return rank(key, node.left);
+         if( cmp > 0) return 1 + size(node.left) + rank(key, node.right);
+         else return size(node);
     }
     
     
@@ -74,6 +98,26 @@ public final class BSTST<Key extends Comparable<Key>, Value> {
         if(t != null) return t;
         return x;
         
+    }
+    
+    public Key min() {
+        Node x = root;
+        if (x == null) return null;
+        while(x.left != null) {
+            x = x.left;
+        }
+        return x.key;
+        
+    }
+    
+    public Key max() {
+        Node x = root;
+        if (x == null) return null;
+        while(x.right != null) {
+            x = x.right;
+            
+        }
+        return x.key;
     }
     public void delte(Key key) {
         
